5ae984c6afd961de3254d16325fe18548b3c5033
edelynnm <edelynnm@gmail.com>
2020-12-11 13:10:32 +0800

    refactor: refactor login

--- server/src/Routes/auth.js
+++ server/src/Routes/auth.js
@@ -1,11 +1,14 @@
 import express from "express";
 import bcrypt from "bcrypt";
 import crypto from "crypto";
+import jwt from "jsonwebtoken";
+import dotenv from "dotenv";
 import sendEmail from "../utils/sendEmail.js";
 import pgClient from "../db.js";
 
-const router = express.Router();
+dotenv.config();
 
+const router = express.Router();
 router.post("/signUp", async (req, res) => {
   try {
     const {
@@ -24,12 +27,12 @@ router.post("/signUp", async (req, res) => {
       });
     }
 
-    const verificationToken = crypto.randomBytes(64).toString("hex");
+    const verificationToken = crypto.randomBytes(128).toString("hex");
     const businesses = await pgClient.query(
       "INSERT INTO businesses (business_name) VALUES ($1) RETURNING business_id;",
       [businessName],
     );
-    const hashPassword = await bcrypt.hash(password, 13);
+    const hashPassword = await bcrypt.hash(password, 10);
     await pgClient.query(
       "INSERT INTO users (user_role_id, user_business_id, email, password, fname, lname, verification_token) VALUES ($1, $2, $3, $4, $5, $6, $7);",
       [
@@ -60,18 +63,38 @@ router.post("/signUp", async (req, res) => {
 router.patch("/signUp/verifyEmail", async (req, res) => {
   try {
     const verificationToken = req.query.token;
-    const result = await pgClient.query("SELECT * FROM users WHERE verification_token = $1", [verificationToken]);
-    if (result.rowCount === 0) {
-      return res.send(400);
+    const { rows } = await pgClient.query("SELECT * FROM users WHERE verification_token = $1", [verificationToken]);
+    if (rows.length === 0) {
+      return res.send(400).json({ success: false, message: "Invalid request." });
     }
-    if (result.rows[0].is_verified) {
-      return res.json({ success: false, message: "Email has already been verified" });
+    if (rows[0].is_verified) {
+      return res.status(409).json({ success: false, message: "Email has already been verified" });
     }
-    await pgClient.query("UPDATE users SET is_verified = true WHERE user_id = $1", [result.rows[0].user_id]);
+    await pgClient.query("UPDATE users SET is_verified = true WHERE user_id = $1", [rows[0].user_id]);
     return res.json({ success: true, message: "Email verified successfully!" });
   } catch (error) {
     console.log(error);
-    return res.send(500);
+    return res.sendStatus(500);
+  }
+});
+
+router.post("/login", async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const { rows } = await pgClient.query("SELECT user_id, user_role_id, user_business_id, fname, lname, email, password, is_verified FROM users WHERE email = $1 AND is_verified = true;", [email]);
+    if (rows.length === 0) {
+      res.status(400).json({ success: false, message: "Account not found." });
+    }
+    const validPassword = await bcrypt.compare(password, rows[0].password);
+    if (!validPassword) {
+      res.status(401).json({ success: false, message: "Incorrect email/password" });
+    }
+    Reflect.deleteProperty(rows[0], "password");
+    const token = jwt.sign(rows[0], process.env.JWT_SECRET, { algorithm: "HS256", expiresIn: "30m" });
+    return res.json({ success: true, token });
+  } catch (error) {
+    console.log(error);
+    return res.sendStatus(500);
   }
 });
 
