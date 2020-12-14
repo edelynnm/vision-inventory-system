581553b48d4b09c9f6660e9b0600cdfdf01b8184
edelynnm <edelynnm@gmail.com>
2020-12-14 19:04:19 +0800

    feat: add create new product

--- server/src/Routes/inventory.js
+++ server/src/Routes/inventory.js
@@ -5,6 +5,49 @@ import pgClient from "../db.js";
 const router = express.Router();
 
 router
+  // New item
+  .post("/inventory/add-new-product", authenticateToken, async (req, res) => {
+    try {
+      if ([2, 3].includes(req.user.user_role_id)) {
+        return res
+          .status(401)
+          .json({ message: "You don't have permission to visit this page." });
+      }
+      const {
+        productCode,
+        productCategoryCode,
+        productBrand,
+        productSpecs,
+        productQty,
+        productUnitPrice,
+        productUnit,
+        reorderPoint,
+      } = req.body;
+      const query = {
+        text:
+          "INSERT INTO products (product_code, product_category_code, product_brand, product_specs, product_qty, product_unit_price, product_unit, reorder_point) VALUES ($1, $2, $3, $4, $5, $6, $7, $8)",
+        values: [
+          productCode,
+          productCategoryCode,
+          productBrand,
+          productSpecs,
+          productQty,
+          productUnitPrice,
+          productUnit,
+          reorderPoint,
+        ],
+      };
+
+      await pgClient.query(query);
+      return res.json({
+        success: true,
+        message: "New product added to inventory",
+      });
+    } catch (error) {
+      console.log(error);
+      return res.sendStatus(500);
+    }
+  })
   // New Category
   .post("/inventory/add-new-category", authenticateToken, async (req, res) => {
     try {
@@ -35,3 +78,20 @@ router
   });
 
 export default router;
+
+/*
+QUERYING PARENT CATEGORIES WITH CHILD:
+const firstQ =  SELECT p.category_name AS parent_category, c.category_name as child_category FROM categories as p INNER JOIN categories as c ON  q.category_code = c.parent_category_code ORDER BY parent_category ASC
+
+THEN
+filter firstQ result, sort alphabetically
+const secondQ = firstQ.rows.filter(category => category.parentName === reqParenntName)
+
+SELECTING PARENT CATEGORIES:
+
+SELECT category_code, category_name FROM categories WHERE parent_category_code is null
+;
+
+SELECTING ALL CHILD CATEGORIES [propbably wont use]
+SELECT category_code, category_name from categories where parent_category_code is not null
+*/
