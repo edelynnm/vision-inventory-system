cd872f5d6a4e6661969f53ce33a23fddec071898
edelynnm <edelynnm@gmail.com>
2020-12-26 18:38:36 +0800

    feat: add sales features to index

--- server/src/index.js
+++ server/src/index.js
@@ -1,10 +1,12 @@
 import express from "express";
 import bodyParser from "body-parser";
+import cors from "cors";
 import pool from "./db.js";
 import auth from "./Routes/auth.js";
 import inventory from "./Routes/inventory.js";
 import employees from "./Routes/employees.js";
-import authenticakeToken from "./utils/authenticakeToken.js";
+import authenticateToken from "./utils/authenticateToken.js";
+import sales from "./Routes/sales.js";
 
 const app = express();
 const port = 8000;
@@ -20,10 +22,12 @@ app
   .use(bodyParser.json())
   .use(bodyParser.urlencoded({ extended: true }))
   .use(express.static("public"))
+  .use(cors())
   .use("/api/auth", auth)
-  .use(authenticakeToken)
-  .use("/api", inventory)
-  .use("/api", employees)
+  .use(authenticateToken)
+  .use("/api/inventory", inventory)
+  .use("/api/sales", sales)
+  .use("/api/manage-employees", employees)
 
   .listen(port, () => {
     console.log("Server has started: http://localhost:8000");
