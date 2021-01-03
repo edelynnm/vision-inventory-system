15af72e1d0603623593921ff08b689fd5d6f1cfe
edelynnm <edelynnm@gmail.com>
2021-01-03 10:36:50 +0800

    refactor: refactor theme

--- client/src/Theme/index.js
+++ client/src/Theme/index.js
@@ -5,12 +5,13 @@ const theme = responsiveFontSizes(createMuiTheme({
   palette: {
     primary: {
       main: "#0c30cc",
-      dark: "#00089a",
-      light: "#f0f3f9"
+      dark: "#0b2696",
+      light: "#f4f6fa"
     },
     secondary: {
       main: "#ff592b"
-    }
+    },
+    divider: "#e4e9f4"
   },
   typography: {
     fontFamily: [
@@ -24,6 +25,13 @@ const theme = responsiveFontSizes(createMuiTheme({
   },
   shape:{
     borderRadius:8
+  },
+  overrides: {
+    MuiDrawer: {
+      paper: {
+        width: 240,
+      },
+    }
   }
 }));
 
