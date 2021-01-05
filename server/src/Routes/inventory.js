c6ad662d464f801d20c447bda9e0c6fe126f84f8
edelynnm <edelynnm@gmail.com>
2021-01-06 01:28:38 +0800

    refactor: clean up and remove remaining category related feats

--- server/src/Routes/inventory.js
+++ server/src/Routes/inventory.js
@@ -5,19 +5,16 @@ import authenticateRole from "../utils/authenticateRole.js";
 const router = express.Router();
 
 router
-  // Display all items + search by item name - fetch at inventory page selection
+  // Display all items
   .get("/", async (req, res) => {
     try {
       const query = {
-        text: // `SELECT * from items`
+        text:
         `SELECT item_code as id,
         CONCAT_WS(' ', item_brand, item_specs) AS item_desc,
         item_qty, item_unit_price,
-        item_unit, reorder_point,
-        C.category_name
-        FROM items as I
-        INNER JOIN categories as C ON I.item_category_code = C.category_code
-        -- WHERE item_brand ILIKE $1 OR item_specs ILIKE $1
+        item_unit, reorder_point
+        FROM items
         ORDER BY item_desc ASC;
         `,
       };
