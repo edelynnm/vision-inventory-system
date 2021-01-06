881efe9ed743eb34b67e75a1a4de0c8a0974d5a1
edelynnm <edelynnm@gmail.com>
2021-01-07 06:42:59 +0800

    feat: protect private routes

--- client/src/App.js
+++ client/src/App.js
@@ -2,27 +2,39 @@ import "./App.css";
 import { ThemeProvider } from "@material-ui/core/styles";
 import { Switch, BrowserRouter as Router, Route } from "react-router-dom";
 import theme from "./Theme/index";
-import PrivateRoutes from "./Utils/routes"
-import Login from "./Components/Auth/login"
-import Signup from "./Components/Auth/signup";
-import VerifyEmail from "./Components/Auth/verifyEmail";
+import ProtectedRoutes from "./Utils/routes";
+import Login from "./Components/AuthScreen/login";
+import Signup from "./Components/AuthScreen/signup";
+import VerifyEmail from "./Components/AuthScreen/verifyEmail";
+import { AuthProvider } from "./Utils/auth";
+import PrivateRoute from "./Components/Auth/privateRoute";
 
 function App() {
   return (
-    <ThemeProvider theme={theme}>
-      <Router>
-        <Switch>
-          <Route key="Login" exact path="/" component={Login} />
-          <Route key="Signup" exact path="/auth/signup" component={Signup} />
-          <Route key="VerifyEmail" exact path="/auth/signup/verify-email" component={VerifyEmail} />
-          
-          {PrivateRoutes.map((route) => (
-            <Route key={route.sidebarName} exact path={route.path} component={route.component} />
-          ))}
-          
-        </Switch>
-      </Router>
-    </ThemeProvider>
+    <AuthProvider>
+      <ThemeProvider theme={theme}>
+        <Router>
+          <Switch>
+            <Route key="Login" exact path="/" component={Login} />
+            <Route key="Signup" exact path="/auth/signup" component={Signup} />
+            <Route
+              key="VerifyEmail"
+              exact
+              path="/auth/signup/verify-email"
+              component={VerifyEmail}
+            />
+            {ProtectedRoutes.map((route) => (
+              <PrivateRoute
+                key={route.sidebarName}
+                exact
+                path={route.path}
+                component={route.component}
+              />
+            ))}
+          </Switch>
+        </Router>
+      </ThemeProvider>
+    </AuthProvider>
   );
 }
 
