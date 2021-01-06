7d1bfd6411a6f4191884ad2d4b61a8577f67debc
edelynnm <edelynnm@gmail.com>
2021-01-07 06:46:10 +0800

    refactor: refactor code

--- server/src/Routes/auth.js
+++ server/src/Routes/auth.js
@@ -5,11 +5,11 @@ import jwt from "jsonwebtoken";
 import dotenv from "dotenv";
 import sendEmail from "../utils/sendEmail.js";
 import pgClient from "../db.js";
-import authenticateToken from "../utils/authenticakeToken.js";
 
 dotenv.config();
 
 const router = express.Router();
+
 router.post("/signup", async (req, res) => {
   try {
     const verificationToken = crypto.randomBytes(64).toString("hex");
@@ -47,7 +47,7 @@ router.post("/signup", async (req, res) => {
       ],
     );
 
-    const verificationMsg = `<p>Hello ${fname}!<br>Please verify that your email is ${email}, and that you entered it when signing up for VISION Inventory Management System.<a href="http://localhost:8000/auth/signup/verify-email?token=${verificationToken}"><br><input type="submit" value="Verify Email"/></a></p>`;
+    const verificationMsg = `<p>Hello ${fname}!<br>Please verify that your email is ${email}, and that you entered it when signing up for VISION Inventory Management System.<a href="http://localhost:3000/auth/signup/verify-email?token=${verificationToken}"><br><input type="submit" value="Verify Email"/></a></p>`;
     const subject = "Email Verification for VISION Inventory";
     sendEmail(verificationMsg, subject, email);
 
@@ -66,20 +66,18 @@ router.patch("/signup/verify-email", async (req, res) => {
     const reqVerificationToken = req.query.token;
     const { rows } = await pgClient.query("SELECT * FROM users WHERE verification_token = $1", [reqVerificationToken]);
     if (rows.length === 0) {
-      return res.status(400).json({ success: false, message: "Invalid request." });
+      return res.json({ success: false, message: "Invalid request." });
     }
     if (rows[0].is_verified) {
-      return res.status(409).json({ success: false, message: "Email has already been verified" });
+      return res.json({ success: false, message: "Email has already been verified" });
     }
 
     const users = await pgClient.query("UPDATE users SET is_verified = true WHERE user_id = $1 RETURNING *", [rows[0].user_id]);
 
-    if (users.rows[0].user_id !== 1) {
+    if (users.rows[0].user_role_id !== 1) {
       const user = users.rows[0];
       ["password", "verification_token"].forEach((prop) => delete user[prop]);
-      const token = jwt.sign(user, process.env.JWT_SECRET, { algorithm: "HS256", expiresIn: "10m" });
-      return res.json({ success: true, token });
-      // return res.redirect(`employee?token=${reqVerificationToken}`);
+      return res.json({ success: true, message: "Redirecting to employee sign in" });
     }
 
     return res.json({ success: true, message: "Email verified successfully!" });
@@ -92,16 +90,21 @@ router.patch("/signup/verify-email", async (req, res) => {
 router.post("/login", async (req, res) => {
   try {
     const { email, password } = req.body;
-    const { rows } = await pgClient.query("SELECT user_id, user_role_id, user_business_id, fname, lname, email, password, is_verified FROM users WHERE email = $1 AND is_verified = true;", [email]);
+
+    const { rows } = await pgClient.query("SELECT user_id, user_role_id, user_business_id, fname, lname, email, password, is_verified FROM users WHERE email = $1", [email]);
     if (rows.length === 0) {
-      res.status(400).json({ success: false, message: "Account not found." });
+      return res.json({ success: false, message: "Account not found." });
     }
+    if (rows.length === 1 && !rows[0].is_verified) {
+      return res.json({ success: false, message: "Account not verified" });
+    }
+
     const validPassword = await bcrypt.compare(password, rows[0].password);
     if (!validPassword) {
-      res.status(401).json({ success: false, message: "Incorrect email/password" });
+      return res.json({ success: false, message: "Incorrect email/password" });
     }
     Reflect.deleteProperty(rows[0], "password");
-    const token = jwt.sign(rows[0], process.env.JWT_SECRET, { algorithm: "HS256" }); // EDIT BACK TO 30 mins
+    const token = jwt.sign(rows[0], process.env.JWT_SECRET, { algorithm: "HS256", expiresIn: "2h" });
     return res.json({ success: true, token });
   } catch (error) {
     console.log(error);
@@ -109,19 +112,18 @@ router.post("/login", async (req, res) => {
   }
 });
 
-router.patch("/signup/employee", authenticateToken, async (req, res) => {
+router.patch("/signup/employee", async (req, res) => {
   try {
     const { password } = req.body;
-
     const hashedPassword = await bcrypt.hash(password, 10);
-    await pgClient.query("UPDATE users SET password = $1 WHERE user_id = $2", [hashedPassword, req.user.user_id]);
+    console.log(hashedPassword);
+    await pgClient.query("UPDATE users SET password = $1 WHERE verification_token = $2", [hashedPassword, req.query.token]);
     return res.json({ success: true, message: "Password successfully changed." });
-    // TODO: invoke jwt token before exp
   } catch (error) {
     console.log(error);
     return res.sendStatus(500);
   }
 });
 
-// TODO: logout & 2 social logins
+// TODO: social logins
 export default router;
