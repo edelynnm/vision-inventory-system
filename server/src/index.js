f92c9ef0078c97094222fdcc190a3663173e5f15
edelynnm <edelynnm@gmail.com>
2021-01-08 06:20:33 +0800

    feat: add user api to index.js

--- server/src/index.js
+++ server/src/index.js
@@ -7,6 +7,7 @@ import inventory from "./Routes/inventory.js";
 import employees from "./Routes/employees.js";
 import authenticateToken from "./utils/authenticateToken.js";
 import sales from "./Routes/sales.js";
+import user from "./Routes/user.js";
 
 const app = express();
 const port = 8000;
@@ -25,72 +26,11 @@ app
   .use(cors())
   .use("/api/auth", auth)
   .use(authenticateToken)
+  .use("/api/user", user)
   .use("/api/inventory", inventory)
   .use("/api/sales", sales)
-  .use("/api/manage-employees", employees)
+  .use("/api/employees", employees)
 
   .listen(port, () => {
     console.log("Server has started: http://localhost:8000");
   });
-
-// AUTHENTICATION
-// admin owner login
-
-// .post("/api/login", (req, res) => {
-//   login(pgClient, req, res);
-// })
-
-// // admin owner sign up
-// .post("/api/sign-up", async (req, res) => {
-//   signUp(pgClient, res, req);
-// })
-
-// // verify signUp
-// .post("/api/sign-up/verify-email", (req, res) => {
-//   verifyEmail(pgClient, req, res);
-// })
-
-// // employee sign up by admin
-// .post("/api/manage-emp/reg", (req, res) => {
-//   registerEmployee(pgClient, req, res);
-// })
-
-// .post("/api/sign-up/forgot-password", (req, res) => {
-//   verifyEmail(pgClient, req, res);
-//   const { token } = req.query;
-
-//   pgClient.query("SELECT user_id, verification_token FROM users WHERE verification_token = $1", [token], (pgError, { rows }) => {
-//     if (pgError) {
-//       console.log(pgError);
-//       res.send(500).json({ pgError });
-//     } else if (rows[0] === undefined) {
-//       res.send(400).json({ sucess: false, message: "ERROR: Bad request" });
-//     } else {
-//       res.json(rows[0]);
-//     }
-//   });
-// })
-// // emp setup account
-// .post("/api/sign-up/emp/forgot-password", (req, res) => {
-//   verifyEmail(pgClient, req, res);
-
-//   const { newPassword } = req.body;
-
-//   bcrypt.hash(newPassword, 13, (bcryptErr, hashedPassword) => {
-//     if (bcryptErr) {
-//       console.log(bcryptErr);
-//       res.send(500);
-//     } else {
-//       pgClient.query("UPDATE user SET password = $1 WHERE user_id = $2",
-//         [hashedPassword, req.query.id],
-//         (pgError) => {
-//           if (pgError) {
-//             console.log(pgError);
-//             res.send(500);
-//           } else {
-//             res.json({ success: true, message: "Password changed successfully!" });
-//           }
-//         });
-//     }
-//   });
-// })
