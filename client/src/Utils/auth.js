f5e3e03bc3947f941951c83e969b0a13dae1a6fd
edelynnm <edelynnm@gmail.com>
2021-01-08 06:26:09 +0800

    feat: frontend authorization and login
    authProvider

--- client/src/Utils/auth.js
+++ client/src/Utils/auth.js
@@ -1,43 +1,79 @@
+import ms from "ms";
 import { useState, useContext, createContext, useEffect } from "react";
+import ajax from "./facade";
 
 const authContext = createContext();
 
 // handle user state
-const useProvideAuth = () => { 
-  const [user, setUser] = useState(false)
+const useProvideAuth = () => {
+  const [user, setUser] = useState(false);
+  const [token, setToken] = useState("");
 
   useEffect(() => {
-    fetchToken()
-  },[user])
-  
-  const fetchToken = () => {
-    const token = localStorage.getItem("token");
-    setUser(token);
-  }
+    const isLoggedIn = () => {
+      const expiration = getExpiration();
+      return Date.now < expiration ? true : false;
+    };
 
-  const login = (token) => {
-    localStorage.setItem("token", JSON.stringify(token));
-    setUser(token)
-  }
+    const fetchUser = () => {
+      if (isLoggedIn) {
+        const token = localStorage.getItem("token");
+        setToken(token)
+        getUser(token)
+      } else {
+        alert("Session expired, please login again.")
+        logout();
+      }
+    };
+
+    const getUser = (token) => {
+      ajax.GET({
+        url:"http://localhost:8000/api/user",
+        authToken: token,
+        callback: setUser
+      })
+    }
+
+    fetchUser() 
+    const interval =  setInterval(() => isLoggedIn(), 5000);
+    return () => {
+      console.log('token expired')
+      fetchUser()
+      clearInterval(interval)
+    }
+  }, []);
+
+  const getExpiration = () => JSON.parse(localStorage.getItem("expires_in"));
+
+  const login = (authResult) => {
+    const issuedAt = Date.now();
+    const expiresIn = issuedAt + ms(authResult.expiresIn);
+
+    localStorage.setItem("token", authResult.token);
+    localStorage.setItem("expires_in", expiresIn);
+    window.location.reload();
+  };
 
   const logout = () => {
     localStorage.removeItem("token");
-    setUser(null)
-  }
+    localStorage.removeItem("expires_in");
+    setUser(false);
+    window.location.reload();
+  };
 
-  return {user, login, logout}
-}
+  return { user, token, login, logout };
+};
 
 // Component to provide the value to be inherited
 // useAuth() to read the value
 const AuthProvider = ({ children }) => {
-  const auth = useProvideAuth()
+  const auth = useProvideAuth();
   return <authContext.Provider value={auth}>{children}</authContext.Provider>;
-}
+};
 
 // subscribe && rerender everytime there is change to user
 const useAuth = () => {
-  return useContext(authContext)
-}
+  return useContext(authContext);
+};
 
-export {useAuth, AuthProvider};
+export { useAuth, AuthProvider };
