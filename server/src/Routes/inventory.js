7198afd6cf58987eea7068683246af752b8875a5
edelynnm <edelynnm@gmail.com>
2021-01-08 06:17:00 +0800

    feat: restock api

--- server/src/Routes/inventory.js
+++ server/src/Routes/inventory.js
@@ -9,8 +9,7 @@ router
   .get("/", async (req, res) => {
     try {
       const query = {
-        text:
-        `SELECT item_code as id,
+        text: `SELECT item_code,
         CONCAT_WS(' ', item_brand, item_specs) AS item_desc,
         item_qty, item_unit_price,
         item_unit, reorder_point
@@ -25,7 +24,7 @@ router
       return res.sendStatus(500);
     }
   })
-  .use(authenticateRole([2, 3]))
+  .use(authenticateRole([2]))
   .post("/new-item", async (req, res) => {
     try {
       const {
@@ -38,8 +37,7 @@ router
         reorderPoint,
       } = req.body;
       const query = {
-        text:
-          `INSERT INTO items
+        text: `INSERT INTO items
            (item_code, item_brand, item_specs, item_qty,
            item_unit_price, item_unit, reorder_point)
            VALUES ($1, $2, $3, $4, $5, $6, $7);`,
@@ -53,14 +51,39 @@ router
           reorderPoint,
         ],
       };
-      const { rows } = await pgClient.query("SELECT * FROM items WHERE item_code = $1", [itemCode]);
+      const {
+        rows,
+      } = await pgClient.query("SELECT * FROM items WHERE item_code = $1", [
+        itemCode,
+      ]);
 
-      if (rows.length !== 0) return res.status(409).json({ success: false, message: "Item already exists." });
+      if (rows.length !== 0) {
+        return res.json({ success: false, message: "Item already exists." });
+      }
 
       await pgClient.query(query);
       return res.json({
         success: true,
         message: "New item added to inventory",
+      });
+    } catch (error) {
+      console.log(error);
+      return res.sendStatus(500);
+    }
+  })
+  .post("/restock", async (req, res) => {
+    try {
+      const { itemQty, itemCode } = req.body;
+
+      await pgClient.query("UPDATE items SET item_qty = item_qty + $1 WHERE item_code = $2;",
+        [itemQty, itemCode]);
+
+      await pgClient.query("INSERT INTO restock_records (additional_qty, item_code, user_id) VALUES ($1, $2, $3);",
+        [itemQty, itemCode, req.user.user_id]);
+
+      return res.json({
+        success: true,
+        message: "Item successfully restocked",
       });
     } catch (error) {
       console.log(error);
