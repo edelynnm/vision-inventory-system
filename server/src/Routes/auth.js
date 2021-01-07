8624854bff2554132a81b4a8881f715684835c3a
edelynnm <edelynnm@gmail.com>
2021-01-08 06:18:55 +0800

    feat: add expiresIn key to login response

--- server/src/Routes/auth.js
+++ server/src/Routes/auth.js
@@ -64,20 +64,34 @@ router.post("/signup", async (req, res) => {
 router.patch("/signup/verify-email", async (req, res) => {
   try {
     const reqVerificationToken = req.query.token;
-    const { rows } = await pgClient.query("SELECT * FROM users WHERE verification_token = $1", [reqVerificationToken]);
+    const {
+      rows,
+    } = await pgClient.query(
+      "SELECT * FROM users WHERE verification_token = $1",
+      [reqVerificationToken],
+    );
     if (rows.length === 0) {
       return res.json({ success: false, message: "Invalid request." });
     }
     if (rows[0].is_verified) {
-      return res.json({ success: false, message: "Email has already been verified" });
+      return res.json({
+        success: false,
+        message: "Email has already been verified",
+      });
     }
 
-    const users = await pgClient.query("UPDATE users SET is_verified = true WHERE user_id = $1 RETURNING *", [rows[0].user_id]);
+    const users = await pgClient.query(
+      "UPDATE users SET is_verified = true WHERE user_id = $1 RETURNING *",
+      [rows[0].user_id],
+    );
 
     if (users.rows[0].user_role_id !== 1) {
       const user = users.rows[0];
       ["password", "verification_token"].forEach((prop) => delete user[prop]);
-      return res.json({ success: true, message: "Redirecting to employee sign in" });
+      return res.json({
+        success: true,
+        message: "Redirecting to employee sign in",
+      });
     }
 
     return res.json({ success: true, message: "Email verified successfully!" });
@@ -91,21 +105,35 @@ router.post("/login", async (req, res) => {
   try {
     const { email, password } = req.body;
 
-    const { rows } = await pgClient.query("SELECT user_id, user_role_id, user_business_id, fname, lname, email, password, is_verified FROM users WHERE email = $1", [email]);
+    const { rows } = await pgClient.query(`
+      SELECT user_id, user_role_id, user_business_id, fname, lname, email, password, is_verified
+      FROM users
+      WHERE email = $1`,
+    [email]);
+    console.log(req.body);
+
     if (rows.length === 0) {
       return res.json({ success: false, message: "Account not found." });
     }
+
     if (rows.length === 1 && !rows[0].is_verified) {
       return res.json({ success: false, message: "Account not verified" });
     }
 
     const validPassword = await bcrypt.compare(password, rows[0].password);
+
     if (!validPassword) {
       return res.json({ success: false, message: "Incorrect email/password" });
     }
+
     Reflect.deleteProperty(rows[0], "password");
-    const token = jwt.sign(rows[0], process.env.JWT_SECRET, { algorithm: "HS256", expiresIn: "2h" });
-    return res.json({ success: true, token });
+    const expiresIn = "2h";
+    const token = jwt.sign(rows[0], process.env.JWT_SECRET, {
+      algorithm: "HS256",
+      expiresIn,
+    });
+
+    return res.json({ success: true, token, expiresIn });
   } catch (error) {
     console.log(error);
     return res.sendStatus(500);
@@ -116,14 +144,19 @@ router.patch("/signup/employee", async (req, res) => {
   try {
     const { password } = req.body;
     const hashedPassword = await bcrypt.hash(password, 10);
-    console.log(hashedPassword);
-    await pgClient.query("UPDATE users SET password = $1 WHERE verification_token = $2", [hashedPassword, req.query.token]);
-    return res.json({ success: true, message: "Password successfully changed." });
+
+    await pgClient.query(
+      "UPDATE users SET password = $1 WHERE verification_token = $2",
+      [hashedPassword, req.query.token],
+    );
+    return res.json({
+      success: true,
+      message: "Password successfully changed.",
+    });
   } catch (error) {
     console.log(error);
     return res.sendStatus(500);
   }
 });
 
-// TODO: social logins
 export default router;
