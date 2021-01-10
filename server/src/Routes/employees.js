7bb619786a97d3a15b7d887bba12c712bb11c471
edelynnm <edelynnm@gmail.com>
2021-01-10 17:35:50 +0800

    feat: add get role, get employees and authenticateRole

--- server/src/Routes/employees.js
+++ server/src/Routes/employees.js
@@ -3,14 +3,32 @@ import bcrypt from "bcrypt";
 import crypto from "crypto";
 import sendEmail from "../utils/sendEmail.js";
 import pgClient from "../db.js";
+import authenticateRole from "../utils/authenticateRole.js";
 
 const router = express.Router();
 
 router
+  .use(authenticateRole([1]))
+  .get("/", async (req, res) => {
+    try {
+      const { rows } = await pgClient.query("SELECT U.fname, U.lname, U.email, R.role_title FROM users AS U INNER JOIN roles AS R ON U.user_role_id = R.role_id;");
+      return res.json(rows);
+    } catch (error) {
+      console.log(error);
+      return res.sendStatus(500);
+    }
+  })
+  .get("/roles", async (req, res) => {
+    try {
+      const { rows } = await pgClient.query("SELECT * FROM roles");
+      return res.json(rows);
+    } catch (error) {
+      console.log(error);
+      return res.sendStatus(500);
+    }
+  })
   .post("/register", async (req, res) => {
     try {
-      if (req.user.user_role_id !== 1) return res.status(401).json({ message: "You don't have permission to visit this page." });
-
       const {
         userRoleId,
         email,
@@ -18,6 +36,18 @@ router
         fname,
         lname,
       } = req.body;
+
+      const users = await pgClient.query(
+        "SELECT email FROM users WHERE email = $1;",
+        [email],
+      );
+
+      if (users.rowCount > 0) {
+        return res.json({
+          success: false,
+          message: "Email has already been used.",
+        });
+      }
 
       const verificationToken = crypto.randomBytes(64).toString("hex");
       const hashedPassword = await bcrypt.hash(defaultPassword, 10);
@@ -31,7 +61,7 @@ router
       const invMsg = `
       <p>Hello ${fname}!
       <br>You have been invited to VISION Inventory Management System by ${req.user.fname} ${req.user.lname}.
-      <br><a href="http://localhost:8000/api/signup/verify-email?token=${verificationToken}">
+      <br><a href="http://localhost:3000/auth/verify-email?token=${verificationToken}&type=emp">
       <br><input type="submit" value="Accept Invitation"/></a>
       </p>`;
       const subject = "Account Invitation for VISION Inventory";
