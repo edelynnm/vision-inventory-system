5549a93a4fa316b7bbc5b844ab8df11e200d3cce
edelynnm <edelynnm@gmail.com>
2021-01-10 17:43:03 +0800

    refactor: move into subcomponents folder and remove component prop

--- client/src/Components/Subcomponents/routes.js
+++ client/src/Components/Subcomponents/routes.js
@@ -1,44 +1,34 @@
-import { DashboardRounded, LocalMallRounded, SupervisorAccountRounded, TrendingUpRounded, ExitToAppRounded, LocalOfferRounded } from "@material-ui/icons";
-import Dashboard from "../Components/dashboard";
-import Inventory from "../Components/Inventory/inventory"
-import Sales from "../Components/sales";
-import Employees from "../Components/employees";
-import Reports from "../Components/reports"
+import { DashboardRounded, LocalMallRounded, SupervisorAccountRounded, TrendingUpRounded, LocalOfferRounded } from "@material-ui/icons";
 
 const ProtectedRoutes = [
   {
     sidebarName: "Dashboard",
     icon: <DashboardRounded />,
     path: "/dashboard",
-    component: Dashboard,
     forbiddenRoleIDs: []
   },
   {
     sidebarName: "Inventory",
     icon: <LocalMallRounded />,
     path: "/inventory",
-    component: Inventory,
     forbiddenRoleIDs: [] // provide local forbidden roles for new item & restock feat
   },
   {
     sidebarName: "Sales",
     icon: <LocalOfferRounded />,
     path: "/sales",
-    component: Sales,
     forbiddenRoleIDs: [3]
   },
   {
     sidebarName: "Reports",
     icon: <TrendingUpRounded />,
     path: "/reports",
-    component: Reports,
     forbiddenRoleIDs: [2,3]
   },
   {
     sidebarName: "Employees",
     icon: <SupervisorAccountRounded />,
     path: "/employees",
-    component: Employees,
     forbiddenRoleIDs: [2,3]
   }
 ];
