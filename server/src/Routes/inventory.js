e1d249fa2cdad0893cfcaa7a0685bb6180d3decd
edelynnm <edelynnm@gmail.com>
2021-01-11 13:26:20 +0800

    feat: add restock records api

--- server/src/Routes/inventory.js
+++ server/src/Routes/inventory.js
@@ -8,7 +8,8 @@ router
   // Display all items
   .get("/", async (req, res) => {
     try {
-      const { rows } = await pgClient.query("SELECT * FROM items;");
+      const { rows } = await pgClient.query("SELECT * FROM items WHERE business_id = $1 ORDER BY date_time DESC;", [req.user.user_business_id]);
+
       return res.json(rows);
     } catch (error) {
       console.log(error);
@@ -27,12 +28,14 @@ router
         itemUnit,
         reorderPoint,
       } = req.body;
+
       const query = {
         text: `INSERT INTO items
-           (item_code, item_brand, item_specs, item_qty,
+           (business_id, item_code, item_brand, item_specs, item_qty,
            item_unit_price, item_unit, reorder_point)
-           VALUES ($1, $2, $3, $4, $5, $6, $7);`,
+           VALUES ($1, $2, $3, $4, $5, $6, $7, $8);`,
         values: [
+          req.user.user_business_id,
           itemCode,
           itemBrand,
           itemSpecs,
@@ -42,9 +45,9 @@ router
           reorderPoint,
         ],
       };
-      const {
-        rows,
-      } = await pgClient.query("SELECT * FROM items WHERE item_code = $1", [
+
+      // new item validation
+      const { rows } = await pgClient.query("SELECT * FROM items WHERE item_code = $1", [
         itemCode,
       ]);
 
@@ -66,6 +69,13 @@ router
     try {
       const { itemQty, itemCode } = req.body;
 
+      // item validation
+      const { rows } = await pgClient.query("SELECT * FROM items WHERE item_code = $1", [itemCode]);
+
+      if (rows.length === 0) {
+        return res.json({ success: false, message: "Item does not exist." });
+      }
+
       await pgClient.query("UPDATE items SET item_qty = item_qty + $1 WHERE item_code = $2;",
         [itemQty, itemCode]);
 
@@ -76,6 +86,19 @@ router
         success: true,
         message: "Item successfully restocked",
       });
+    } catch (error) {
+      console.log(error);
+      return res.sendStatus(500);
+    }
+  })
+  .get("/restock-records", async (req, res) => {
+    try {
+      const { rows } = await pgClient.query(`SELECT R.item_code, R.additional_qty, R.restock_date_time, U.fname, U.lname, I.item_brand, I.item_specs, I.item_unit FROM restock_records AS R
+      INNER JOIN items AS I ON I.item_code = R.item_code
+      INNER JOIN users AS U ON U.user_id = R.user_id
+      WHERE U.user_business_id = $1;`, [req.user.user_business_id]);
+
+      return res.json(rows);
     } catch (error) {
       console.log(error);
       return res.sendStatus(500);
