b63f4e53447804bbb4bcb3448463e536223ebd5e
edelynnm <edelynnm@gmail.com>
2021-01-11 13:30:41 +0800

    feat: add daily sales report component

--- client/src/Components/Reports/reports.js
+++ client/src/Components/Reports/reports.js
@@ -1,150 +1,80 @@
 import { Fragment, useEffect, useState } from "react";
-import { makeStyles, withStyles } from "@material-ui/core/styles";
-import {
-  TableContainer,
-  Typography,
-  Table,
-  TableRow,
-  TableBody,
-  TableCell,
-  TableHead,
-  Paper
-} from "@material-ui/core";
-import theme from "../../Theme/index";
+import { Typography} from "@material-ui/core";
 import PageTemplate from "../Subcomponents/uiTemplates/pageTemplate";
 import ajax from "../../Utils/facade";
 import { useAuth } from "../Subcomponents/auth";
+import groupByDate from "../../Utils/groupBy";
+import TableData from "./tableData.";
 
-
-const styles = (theme) => ({
-  margin: {
-    margin: "0 60px 60px 0",
-  },
-  tableContainer: {
-    border: `2px solid ${theme.palette.divider}`,
-    backgroundColor: "white",
-    height: 700,
-  },
-  head: {
-    backgroundColor: theme.palette.primary.light,
-  },
-  displayText: {
-    padding: 200,
-    width: "100",
-  },
-  chevron: {
-    fontSize: "30pt",
-  },
-  backButton: {
-    color: "#7b8ba6",
-    margin: "0px 0 30px -20px",
-    "&:hover": {
-      backgroundColor: "transparent",
-      color: theme.palette.primary.main,
-      transition: "0.3s",
-    },
-  },
-});
-
-const headerStyle = (theme) => ({
-  head: {
-    backgroundColor: theme.palette.primary.light,
-    borderRight: `1px solid ${theme.palette.divider}`,
-    borderBottom: `1px solid ${theme.palette.divider}`,
-  },
-});
-
-const cellStyle = (theme) => ({
-  body: {
-    borderRight: `1px solid ${theme.palette.divider}`,
-    borderBottom: `1px solid ${theme.palette.divider}`,
-  },
-});
-
-const TableHeader = withStyles(headerStyle)(TableCell);
-const StyledTableCell = withStyles(cellStyle)(TableCell);
-const useStyle = makeStyles(styles);
 
 const Reports = () => {
-  const classes = useStyle(theme);
   const auth = useAuth();
-  const [items, setItems ] = useState([]);
+  const [items, setItems] = useState({});
 
-  useEffect(() => {
+  useEffect(() => { 
     fetchItems()
-  },[])
+  }, []);
+
+
+  const handleChange = (body) => { 
+    setItems(groupByDate(body));
+  };
 
   const fetchItems = () => {
     ajax.GET({
       url: "http://localhost:8000/api/report",
       authToken: auth.token,
-      headers: {"Content-Type": "application/json"},
-      callback: setItems
-    })
+      headers: { "Content-Type": "application/json" },
+      callback: handleChange,
+    });
+  };
+  
+  const getTable = (items) => {
+    let data = []
+    for (const [key, value] of Object.entries(items)) {
+      data.push(<TableData date={key} value={value} />)
+    }
+    return data;
   }
+  
   const displayText =
-    items.length === 0 ? (
-      <div className={classes.displayText}>
-        <Typography align="center">Nothing to display</Typography>
-      </div>
-    ) : (
-      ""
-    );
-
+    items.entries === undefined ? (
+    <div styles={{padding: 200, width: "100",}}>
+      <Typography align="center">Nothing to display</Typography>
+    </div>
+  ) : (
+    ""
+  );
 
   const main = (
     <Fragment>
-      <Typography
-        align="left"
-        variant="h5"
-        color="primary"
-        style={{ fontWeight: 700, marginBottom: 30 }}
+      <div
+        style={{
+          height: 100,
+          display: "flex",
+          justifyContent: "space-between",
+          alignItems: "center",
+          marginRight: 60,
+        }}
+      >
+        <Typography
+          align="left"
+          variant="h5"
+          color="primary"
+          style={{ fontWeight: 700 }}
         >
-        Sales Report
-      </Typography>
-      <div className={classes.margin}>
-        <TableContainer
-          component={Paper}
-          elevation={0}
-          className={classes.tableContainer}
-        >
-          <Table stickyHeader >
-            <TableHead>
-              <TableRow>
-                <TableHeader width={120}>Item Code</TableHeader>
-                <TableHeader width={120}>Description</TableHeader>
-                <TableHeader width={100}>Unit</TableHeader>
-                <TableHeader width={80}>Unit Price</TableHeader>
-                <TableHeader width={100}>Total Sold</TableHeader>
-                <TableHeader width={80}>Total Amount</TableHeader>
-              </TableRow>
-            </TableHead>
-            <TableBody>
-            {items.map((item, index) => (
-              <TableRow key={index} hover>
-                <StyledTableCell>{item.item_code}</StyledTableCell>
-                <StyledTableCell>
-                  {`${item.item_brand} ${item.item_specs}`}
-                </StyledTableCell>
-                <StyledTableCell>{item.item_unit}</StyledTableCell>
-                <StyledTableCell>₱ {item.item_unit_price}</StyledTableCell>
-                <StyledTableCell>{item.sold}</StyledTableCell>
-                <StyledTableCell>₱ {Number(item.sold) * Number(item.item_unit_price)}</StyledTableCell>
-
-              </TableRow>
-            ))}
-              </TableBody>
-
-          </Table>
-          {displayText}
-        </TableContainer>
+          Sales Report
+        </Typography>
       </div>
+      {displayText}
+
+        {getTable(items)}
     </Fragment>
-  )
+  );
 
   return (
     <Fragment>
-      <PageTemplate component={main}/>
+      <PageTemplate component={main} />
     </Fragment>
   );
 };
---
+++ client/src/Components/Reports/tableData..js
@@ -0,0 +1,99 @@
+import { makeStyles, withStyles } from "@material-ui/core/styles";
+import {
+  TableContainer,
+  Typography,
+  Table,
+  TableRow,
+  TableBody,
+  TableCell,
+  TableHead,
+  Paper
+} from "@material-ui/core";
+import theme from "../../Theme/index";
+
+const styles = (theme) => ({
+  margin: {
+    margin: "0 60px 60px 0",
+  },
+  tableContainer: {
+    border: `2px solid ${theme.palette.divider}`,
+    backgroundColor: "white",
+  },
+});
+
+const headerStyle = (theme) => ({
+  head: {
+    backgroundColor: theme.palette.primary.light,
+    borderRight: `1px solid ${theme.palette.divider}`,
+    borderBottom: `1px solid ${theme.palette.divider}`,
+  },
+});
+
+const cellStyle = (theme) => ({
+  body: {
+    borderRight: `1px solid ${theme.palette.divider}`,
+    borderBottom: `1px solid ${theme.palette.divider}`,
+  },
+});
+
+const TableHeader = withStyles(headerStyle)(TableCell);
+const StyledTableCell = withStyles(cellStyle)(TableCell);
+const useStyle = makeStyles(styles);
+  
+const TableData = (props) => {
+  const classes = useStyle(theme);
+  
+  const convertToDate = (timestamp) => {
+    const options = {
+      year: "numeric",
+      month: "long",
+      day: "numeric",
+    }
+    const date = new Date(timestamp).toLocaleString(undefined, options);
+    return date
+  }
+
+  return (
+    <div className={classes.margin}>
+      <Typography style={{ fontWeight: 700 }} gutterBottom>
+        {convertToDate(props.date)}
+      </Typography>
+      <TableContainer
+        component={Paper}
+        elevation={0}
+        className={classes.tableContainer}
+      >
+        <Table stickyHeader>
+          <TableHead>
+            <TableRow key={convertToDate(props.date)}>
+              <TableHeader width={120}>Item Code</TableHeader>
+              <TableHeader width={200}>Description</TableHeader>
+              <TableHeader width={80}>Unit</TableHeader>
+              <TableHeader width={80}>Unit Price</TableHeader>
+              <TableHeader width={80}>Total Sold</TableHeader>
+              <TableHeader width={80}>Total Amount</TableHeader>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {props.value.map((item) => (
+              <TableRow hover>
+                <StyledTableCell>{item.item_code}</StyledTableCell>
+                <StyledTableCell>
+                  {`${item.item_brand} ${item.item_specs}`}
+                </StyledTableCell>
+                <StyledTableCell>{item.item_unit}</StyledTableCell>
+                <StyledTableCell>₱ {item.item_unit_price}</StyledTableCell>
+                <StyledTableCell>{item.sold}</StyledTableCell>
+                <StyledTableCell>
+                  ₱ {Number(item.sold) * Number(item.item_unit_price)}
+                </StyledTableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </div>
+  );
+};
+
+export default TableData;
