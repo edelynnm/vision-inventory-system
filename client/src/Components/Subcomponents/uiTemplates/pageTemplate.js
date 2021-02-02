ebca67f556e8762f0c4867b2e1fc101746b7e7e4
edelynnm <edelynnm@gmail.com>
2021-02-03 01:45:55 +0800

    feat: add avatar

--- client/src/Components/Subcomponents/uiTemplates/pageTemplate.js
+++ client/src/Components/Subcomponents/uiTemplates/pageTemplate.js
@@ -1,4 +1,4 @@
-import React, { Fragment, useState } from "react";
+import React, { Fragment, useEffect, useState } from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import {
   Hidden,
@@ -8,12 +8,15 @@ import {
   IconButton,
   Toolbar,
   Box,
-  Typography
+  Typography,
+  Avatar,
+  Badge
 } from "@material-ui/core";
-import { MenuRounded } from "@material-ui/icons"
+import { MenuRounded, NotificationsRounded } from "@material-ui/icons"
 import theme from "../../../Theme/index";
 import CustomDrawer from "./drawer";
 import { useAuth } from "../auth";
+import ajax from "../../../Utils/facade";
 
 const styles = (theme) => ({
   main: {
@@ -32,23 +35,52 @@ const styles = (theme) => ({
       display: "none",
     },
   },
+  avatar: {
+    backgroundColor: theme.palette.secondary.main,
+  }
 });
 
 const useStyle = makeStyles(styles);
 
 const PageTemplate = (props) => {
   const classes = useStyle(theme);
-  const [mobileOpen, setMobileOpen] = useState(false);
   const auth = useAuth();
-  
+  const [mobileOpen, setMobileOpen] = useState(false);
+  // const [restockItems, setRestockItems] = useState(false);
+
+  // useEffect(() => {
+  //   const interval = setInterval(() => {
+  //     getItems();
+  //   }, 1000);
+  //   return () => {
+  //     clearInterval(interval);
+  //   };
+  // }, [])
+
+  // const getItems = () => {
+  //   ajax.GET({
+  //     url: "http://localhost:8000/api/inventory",
+  //     authToken: auth.token,
+  //     callback: getRestockItems,
+  //   });
+  // };
+
+  // const getRestockItems = (items) => {
+  //   const result = items.filter((item) =>
+  //     item.qty <= item.reorder_point ? item : ""
+  //   );
+  //   setRestockItems(result);
+  // };
+
   const handleDrawerToggle = () => {
     setMobileOpen(!mobileOpen);
   };
 
+  const initials = `${auth.user.fname[0]}${auth.user.lname[0]}`
   return (
     <Fragment>
       <AppBar position="fixed" elevation={0} color="primary" className={classes.appBar}>
-        <Toolbar>
+        <Toolbar style={{"display": "flex", "justifyContent": "space-between"}}>
           <IconButton
             color="inherit"
             aria-label="open drawer"
@@ -61,6 +93,17 @@ const PageTemplate = (props) => {
           <Typography variant="h6" noWrap>
             {auth.user.businessName}
           </Typography>
+          <div style={{"display": "flex", "flexDirection":"row"}}>
+          <Avatar className={classes.avatar}>{initials}</Avatar>
+          {/* <Badge
+            overlap="circle"
+            color="error"
+            badgeContent={restockItems.length}>
+            <Avatar style={{"background": "#e5ebf4", "color":"#041d8a", "marginLeft": 8}}>
+           <NotificationsRounded></NotificationsRounded>
+          </Avatar>
+          </Badge> */}
+          </div>
         </Toolbar>
       </AppBar>
       <Hidden xsDown>
